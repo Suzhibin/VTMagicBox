7590933fd3da69d818ae157c8bdfea528596dd15
Andy <szb2323@163.com>
2024-11-07 14:22:31 +0800

    [Update]SMP

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .target(
             name: "VTMagicBox",
             path: "VTMagicBox",
-            publicHeadersPath: "")
+            publicHeadersPath: "../VTMagicBox")
     
     ]
 )
